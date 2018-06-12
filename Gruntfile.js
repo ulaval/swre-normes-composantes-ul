6345450d16de0f48f0997a404175bdf8fb7613c0
Nicolas Pelletier <nicolas.pelletier@bsw.ulaval.ca>
2018-06-12 15:24:35 -0400

    Ajout de composantes et des configurations Grunt

---
+++ Gruntfile.js
@@ -0,0 +1,72 @@
+/********************
+Configurations Grunt des composantes réutilisables
+Bureau des services Web de l'Université Laval
+web@bsw.ulaval.ca
+*********************/
+'use strict';
+module.exports = function (grunt) {
+  require('load-grunt-tasks')(grunt); // Utilise le module load-grunt-tasks pour charger automatiquement les tâches à partir du fichier package.json
+  grunt.initConfig({
+
+    /********************
+    Linters
+    Ils fontionnent tous différemment. Voir le wiki pour des détails.
+    *********************/
+    standard: { // Javascript (https://standardjs.com/)
+        src: ['scripts/**/*.js'] // On scan tout le dossier script et ses sous dossiers. Les configurations sont intégrés dans le module. Utiliser le CLI pour réparer les erreurs automatiquements.
+    },
+    stylelint: { // CSS et SASS (https://stylelint.io/)
+      all: ['css/sass/**/*.scss'] // On scan tout le dossier sass et ses sous dossiers. Les configurations viennent des modules stylelint-config-standard et stylelint-order. Utiliser le CLI pour réparer les erreurs automatiquements.
+    },
+    htmllint: { // HTML et Twig (http://htmllint.github.io/). Non utile dans le cas d'un thème WordPress si aucune intégration statique n'est faite avant d'ajouter le PHP. Les erreurs doivent être réparé manuellement. CLI en développement.
+      options: {
+        htmllintrc: '.htmllintrc', //fichier de configuration
+      },
+      src: ['dossier-des-gabarits/**/*.twig']
+    },
+    /* Fin Linters
+    ********************/
+    sass: {
+      main: {
+        options: {
+          style: 'compressed',
+          unixNewlines: true,
+        },
+        files: {
+          'css/composantes-ul.css': 'css/sass/assemblage.scss',
+        },
+      },
+    },
+    watch: {
+      configFiles: {
+        files: [ 'Gruntfile.js'],
+        options: {
+          reload: true
+        }
+      },
+      sass: {
+        files: ['css/sass/**/*.scss'],
+        tasks: ['stylelint', 'sass:main'],
+      },
+      scripts: {
+        files: ['scripts/**/*.js'],
+        tasks: ['standard'],
+      },
+    },
+  })
+  grunt.registerTask('build', [ // Construit un environnement de test local
+    'ci',
+    'sass:main',
+  ]);
+  grunt.registerTask('serve', [ // Démarre les services nécessaires au développement
+    'build',
+    'watch',
+  ]);
+  grunt.registerTask('ci', [ // Validation du code
+    'standard',
+    'stylelint',
+  ]);
+  grunt.registerTask('default', [ // Exécuté avec la commande "grunt" seule
+    'build'
+  ]);
+};
